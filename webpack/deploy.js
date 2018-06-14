b3867832e2e0966020943952d9fe70b9ff1da2b1
Miki Shiran <mikeyshiran@gmail.com>
2018-06-14 12:13:13 +0300

    Default to 'edge' when no tag is specified. Fail on invalid tag, etc'

--- webpack/deploy.js
+++ webpack/deploy.js
@@ -2,16 +2,41 @@ const semver = require('semver');
 const { execSync } = require('child_process');
 
 const CURRENT_VERSION = require('../package.json').version;
+const VALID_TAGS = ['edge', 'stable'];
 
 const nextEdgeVersion = () => semver.inc(CURRENT_VERSION, 'prerelease', undefined, 'edge');
 const nextStableVersion = () => semver.inc(CURRENT_VERSION, 'patch');
-const nextVersion = () => {
-  const tag = process.argv[2];
-  return (tag === 'edge') ? nextEdgeVersion() : nextStableVersion();
+const nextVersion = (tag) => (tag === 'edge') ? nextEdgeVersion() : nextStableVersion();
+
+const publishCmd = (tag) => {
+  let cmd = 'npm publish';
+
+  if (tag !== 'stable') {
+    cmd += ' --tag edge';
+  }
+
+  return cmd;
 };
 
-const cmd = `npm version ${nextVersion()}`;
+const versionCmd = (tag) => `npm version ${nextVersion(tag)}`;
+
+const extractTag = () => {
+  let tag = process.argv[2];
+
+  if (!tag) {
+    return 'edge';
+  }
+
+  if (!VALID_TAGS.find((t) => t === tag)) {
+    throw Error.new(`Invalid tag ${tag}. Valid tags are: ${VALID_TAGS.join(', ')}`);
+  }
+
+  return tag;
+};
+
+const tag = extractTag();
+const cmd = `${versionCmd(tag)} && ${publishCmd(tag)}`;
 
 console.log(`Executing ${cmd}...`);
 
-execSync(cmd);
+// execSync(cmd);
