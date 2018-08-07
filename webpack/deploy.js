ca129dd9eb750f1c2f2092664500c2c6175fbc90
tsi <tsi.shlidor@gmail.com>
2018-08-07 10:13:19 +0300

    deploy change
    edge: prerelease -> patch
    stable: patch -> minor

--- webpack/deploy.js
+++ webpack/deploy.js
@@ -2,10 +2,10 @@ const semver = require('semver');
 const { execSync } = require('child_process');
 
 const CURRENT_VERSION = require('../package.json').version;
-const VALID_TAGS = ['edge', 'stable'];
+const VALID_TAGS = ['edge', 'stable', 'dry'];
 
-const nextEdgeVersion = () => semver.inc(CURRENT_VERSION, 'prerelease', undefined, 'edge');
-const nextStableVersion = () => semver.inc(CURRENT_VERSION, 'patch');
+const nextEdgeVersion = () => semver.inc(CURRENT_VERSION, 'patch', undefined, 'edge');
+const nextStableVersion = () => semver.inc(CURRENT_VERSION, 'minor');
 const nextVersion = (tag) => (tag === 'edge') ? nextEdgeVersion() : nextStableVersion();
 
 const versionCmd = (tag) => `npm version ${nextVersion(tag)}`;
@@ -35,8 +35,12 @@ const extractTag = () => {
 };
 
 const tag = extractTag();
-const cmd = `${versionCmd(tag)} && ${publishCmd(tag)}`;
 
-console.log(`Executing: "${cmd}" ...`);
-
-execSync(cmd);
+if (tag === 'dry') {
+  console.log(`"edge" will deploy: "${nextEdgeVersion()}"`);
+  console.log(`"stable" will deploy: "${nextStableVersion()}"`);
+} else {
+  const cmd = `${versionCmd(tag)} && ${publishCmd(tag)}`;
+  console.log(`Executing: "${cmd}" ...`);
+  execSync(cmd);
+}
