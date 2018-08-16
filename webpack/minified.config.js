b294487500aeeabc166f2d35700e6244b8ee4a37
Yaniv Aran-Shamir <yaniv@gambaweb.co.il>
2018-08-16 12:34:41 +0300

    fixed hls in minified version

--- webpack/minified.config.js
+++ webpack/minified.config.js
@@ -6,7 +6,7 @@ const { lightFilenamePart } = require('./build-utils');
 // webpack plugins
 const DefinePlugin = require('webpack/lib/DefinePlugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const UglifyJsPlugin = require('webpack/lib/optimize/UglifyJsPlugin');
+const WebpackShellPlugin = require('webpack-shell-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
@@ -28,18 +28,9 @@ module.exports = merge.smart(webpackCommon, {
     }),
     new ExtractTextPlugin(`[name]${lightFilenamePart}.min.css`),
     new OptimizeCssAssetsPlugin({ canPrint: true }),
-    // new BundleAnalyzerPlugin(),
-    new UglifyJsPlugin({
-      compressor: {
-        screw_ie8: true,
-        warnings: false
-      },
-      mangle: true, // NEED TO FIGURE OUT WHY MANGLING DOESN'T WORK.
-      output: {
-        comments: false,
-        screw_ie8: true
-      },
-      sourceMap: false
+    new WebpackShellPlugin({
+      onBuildStart: ['echo "Starting Minification"'],
+      onBuildEnd: ['./node_modules/uglify-js/bin/uglifyjs --ie8 -m -o ./dist/cld-video-player.min.js ./dist/cld-video-player.js']
     })
   ]
 });
