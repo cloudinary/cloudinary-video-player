06320a319d3912888b1a3c3b9706ff97134c4a4d
Yaniv Aran-Shamir <yaniv@gambaweb.co.il>
2018-08-20 21:19:38 +0300

    deploy fixes

--- webpack/deploy.js
+++ webpack/deploy.js
@@ -1,6 +1,7 @@
 const ver = require('./versioning');
 const { execSync } = require('child_process');
 const tag = ver.extractTag();
+let VERSION = (process.env.deploy === 'true') ? JSON.stringify(ver.getNextVersion()) : JSON.stringify(require('../package.json').version);
 const versionCmd = () => `npm version ${VERSION}`;
 const publishCmd = (tag) => {
   let cmd = 'npm publish';
