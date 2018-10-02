0adecd08055fca654fa5a796d22674b40fe5efb9
Yaniv Aran-Shamir <yaniv@gambaweb.co.il>
2018-10-02 12:50:32 +0300

    basic ui tests

--- test/basic-ui.test.js
+++ test/basic-ui.test.js
@@ -1,30 +1,30 @@
 const puppeteer = require('puppeteer');
 const url = require('url');
 const path = require('path');
-describe('app', () => {
-  beforeEach(async () => {
+describe('basic player tests', () => {
+  beforeAll(async () => {
     // const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
+    // const page = await browser.newPage();
+    await page.setViewport({ width: 1280, height: 800 });
     await page.goto('http://localhost:3000/', { waitUntil: 'load' });
+    await page.evaluate(() => {
+      Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
+        get: function() {
+          return !!(this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2);
+        }
+      });
+    });
   }, 10000);
 
   it('video player should load', async () => {
-    page.once('load', async () => {
-      await expect(page).toMatchElement('#example-player');
-    });
+    await expect(page).toMatchElement('#example-player');
   });
   it('Video should play', async () => {
-    await page.setRequestInterception(true);
-    page.on('request', (interceptedRequest) => {
-      let reqUrl = interceptedRequest.url();
-      let parsed = url.parse(reqUrl);
-      let ext = path.extname()
-      if (ext === 'webm') {
-        return true;
-      }
-    })
-    page.once('load', async () => {
-      await expect(page).toClick('button.vjs-big-play-button');
-    });
+    jest.setTimeout(35000);
+    await page.waitForSelector('.vjs-big-play-button');
+    await page.click('#example-player > button.vjs-big-play-button');
+    await page.waitFor(1000);
+    let playing = await page.$eval('#example-player video', el => el.playing);
+    await expect(playing).toBe(true);
   });
-})
+});
