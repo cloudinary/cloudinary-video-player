17e220a9f3a0b00431c43df16e3882b4d93f9639
Tsachi Shlidor <tsi.shlidor@gmail.com>
2019-03-18 17:17:30 +0200

    version query string

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,19 +1,20 @@
+/* eslint no-var: "off", vars-on-top: "off" */
 
 // Get scripts & styles from:
 // `localhost` while developing
 // `unpkg.com` while demoing
 
-const loadScript = function (source, external) {
-  const script = document.createElement('script');
-  const from = external || window.location.hostname === 'localhost' ? '' : 'https://unpkg.com/cloudinary-video-player@edge/dist';
+var loadScript = function (source, external) {
+  var script = document.createElement('script');
+  var from = external || window.location.hostname === 'localhost' ? '' : 'https://unpkg.com/cloudinary-video-player@edge/dist';
   script.type = 'text/javascript';
   script.src = from + source;
   document.getElementsByTagName('head')[0].appendChild(script);
 };
 
-const loadStyle = function (source, external) {
-  const style = document.createElement('link');
-  const from = external || window.location.hostname === 'localhost' ? '' : 'https://unpkg.com/cloudinary-video-player@edge/dist';
+var loadStyle = function (source, external) {
+  var style = document.createElement('link');
+  var from = external || window.location.hostname === 'localhost' ? '' : 'https://unpkg.com/cloudinary-video-player@edge/dist';
   style.type = 'text/css';
   style.rel = 'stylesheet';
   style.href = from + source;
@@ -21,7 +22,23 @@ const loadStyle = function (source, external) {
 };
 
 (function() {
+
+  // Allows testing of various build versions (min/light)
+  var ver = new URLSearchParams(window.location.search).get('ver');
+  if (ver) {
+    // Maintain the 'ver' query param on internal links.
+    window.addEventListener('load', function (e) {
+      Array.from(document.querySelectorAll('a')).forEach(a => {
+        if (a.hostname === location.hostname) {
+          var url = new URL(a.href);
+          url.searchParams.set('ver', ver);
+          a.setAttribute('href', url);
+        }
+      });
+    }, false);
+  }
+
   loadScript('https://cdnjs.cloudflare.com/ajax/libs/cloudinary-core/2.3.0/cloudinary-core-shrinkwrap.js', true);
-  loadStyle('/cld-video-player.css');
-  loadScript('/cld-video-player.js');
+  loadStyle('/cld-video-player' + (ver ? '.' + ver : '') + '.css');
+  loadScript('/cld-video-player' + (ver ? '.' + ver : '') + '.js');
 }());
