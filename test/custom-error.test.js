13dd29246f36a4fe9cea3618e7dfda8595fe9516
Yaniv Aran-Shamir <yaniv@gambaweb.co.il>
2019-06-18 10:52:23 +0300

    error tests

---
+++ test/custom-error.test.js
@@ -0,0 +1,22 @@
+describe('custom error tests', () => {
+  beforeAll(async () => {
+    jest.setTimeout(35000);
+    await page.setViewport({ width: 1280, height: 800 });
+    await page.goto('http://localhost:3000/docs/custom-cld-errors.html', { waitUntil: 'load' });
+    await page.evaluate(() => {
+      Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
+        get: function() {
+          return !!(this.currentTime > 0 && !this.paused && !this.ended &&
+              this.readyState > 2);
+        }
+      });
+    });
+  }, 10000);
+  it('Test error', async () => {
+    jest.setTimeout(35000);
+    await page.waitFor(1000);
+    let errorMsg = await page.evaluate(() => player.videojs.error_.message);
+    let title = await page.$eval('h3.mb-4', (t) => t.innerHTML);
+    expect(title.includes(errorMsg)).toBe(true);
+  });
+});
