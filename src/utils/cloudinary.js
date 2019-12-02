0a11b0614044ea2b166abd88eb154c72f8121261
maoznir <nir.maoz@cloudinary.com>
2019-12-02 17:35:14 +0200

    Update headers for get requests when testing urls

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -5,7 +5,7 @@ import { cloudinaryErrorsConverter } from '../plugins/cloudinary/common';
 const { fetch } = fetchPF({ Promise });
 
 const GET_ERROR_DEFAULT_REQUEST = { method: 'head' };
-const ERROR_WITH_GET_REQUEST = { method: 'get', credentials: 'include', headers: { Range: 'bytes=0-0' } };
+const ERROR_WITH_GET_REQUEST = { method: 'get', credentials: 'include', headers: { 'Content-Range': 'bytes=0-0' } };
 const handleCldError = (that, options) => {
   const cloudName = that.cloudinaryConfig().config().cloud_name;
   const opts = (options.fetchErrorUsingGet) ? ERROR_WITH_GET_REQUEST : GET_ERROR_DEFAULT_REQUEST;
