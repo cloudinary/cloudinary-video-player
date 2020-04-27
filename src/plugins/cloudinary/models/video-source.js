5c96a7736e3a9cad07ece9fbfcfea8c8068bb496
Yaniv Aran-Shamir <yaniv@gambaweb.co.il>
2020-04-27 11:31:12 +0300

    ie 11 fixes

--- src/plugins/cloudinary/models/video-source.js
+++ src/plugins/cloudinary/models/video-source.js
@@ -5,7 +5,56 @@ import { sliceAndUnsetProperties } from 'utils/slicing';
 import assign from 'utils/assign';
 import { objectToQuerystring } from 'utils/querystring';
 
+if (!Array.prototype.find) {
+  // eslint-disable-next-line no-extend-native
+  Object.defineProperty(Array.prototype, 'find', {
+    value: function(predicate) {
+      // 1. Let O be ? ToObject(this value).
+      if (this === null) {
+        // eslint-disable-next-line new-cap
+        throw TypeError('"this" is null or not defined');
+      }
 
+      let o = Object(this);
+
+      // 2. Let len be ? ToLength(? Get(O, "length")).
+      // eslint-disable-next-line no-bitwise
+      let len = o.length >>> 0;
+
+      // 3. If IsCallable(predicate) is false, throw a TypeError exception.
+      if (typeof predicate !== 'function') {
+        // eslint-disable-next-line new-cap
+        throw TypeError('predicate must be a function');
+      }
+
+      // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
+      // eslint-disable-next-line prefer-rest-params
+      let thisArg = arguments[1];
+
+      // 5. Let k be 0.
+      let k = 0;
+
+      // 6. Repeat, while k < len
+      while (k < len) {
+        // a. Let Pk be ! ToString(k).
+        // b. Let kValue be ? Get(O, Pk).
+        // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
+        // d. If testResult is true, return kValue.
+        let kValue = o[k];
+        if (predicate.call(thisArg, kValue, k, o)) {
+          return kValue;
+        }
+        // e. Increase k by 1.
+        k++;
+      }
+
+      // 7. Return undefined.
+      return undefined;
+    },
+    configurable: true,
+    writable: true
+  });
+}
 const DEFAULT_POSTER_PARAMS = { format: 'jpg', resource_type: 'video' };
 const DEFAULT_VIDEO_SOURCE_TYPES = ['webm/vp9', 'mp4/h265', 'mp4'];
 
@@ -229,5 +278,4 @@ function normalizeFormat(format) {
 
   return res;
 }
-
 export default VideoSource;
