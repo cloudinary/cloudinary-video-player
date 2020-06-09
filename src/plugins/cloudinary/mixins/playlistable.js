31ba4a08aa2d6d3d0c36763f4870ee04c70f7976
Tsachi Shlidor <tsi.shlidor@gmail.com>
2020-06-09 14:01:43 +0300

    playlist

--- src/plugins/cloudinary/mixins/playlistable.js
+++ src/plugins/cloudinary/mixins/playlistable.js
@@ -1,11 +1,9 @@
-import Playlist from '../playlist';
+import Playlist from 'components/playlist/playlist';
 import Promise from 'promise-polyfill';
 import fetchPF from 'fetch-ponyfill/build/fetch-browser';
 import { sliceProperties } from 'utils/slicing';
 import { normalizeJsonResponse } from 'utils/api';
 import assign from 'utils/assign';
-import '../components/playlist';
-import '../components/upcoming-video-overlay';
 
 const { fetch } = fetchPF({ Promise });
 
