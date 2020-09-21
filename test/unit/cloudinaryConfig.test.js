5c0ee3d2052e2206f0add398344d5c97169868aa
Yaniv Aran-Shamir <yaniv@gambaweb.co.il>
2020-09-21 10:29:39 +0300

    continue tests

--- test/unit/cloudinaryConfig.test.js
+++ test/unit/cloudinaryConfig.test.js
@@ -1,6 +1,7 @@
 import cloudinary from 'cloudinary-core';
 import VideoPlayer from '../../src/video-player';
-// import videojs from '../../node_modules/video.js';
+import window from 'global/window';
+import videojs from '../../node_modules/video.js';
 // import defaults from '../../src/config/defaults.js';
 const cld = cloudinary.Cloudinary.new({ cloud_name: 'demo' });
 cloudinary.VideoPlayer = VideoPlayer;
@@ -8,6 +9,7 @@ cloudinary.VideoPlayer = VideoPlayer;
 
 describe('secure true test', () => {
   it('test force secure true', () => {
+    jest.useFakeTimers();
     document.body.innerHTML = '<div><video id="test"/></div>';
     let vp = new VideoPlayer('test', { hideContextMenu: true }, false);
     console.log(vp);
