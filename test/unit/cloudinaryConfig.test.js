04ac62a1d6958e1a92f7688cbeec1371a28dcff0
Yaniv Aran-Shamir <yaniv@gambaweb.co.il>
2020-10-12 18:21:38 +0300

    fix runing test from yarn

--- test/unit/cloudinaryConfig.test.js
+++ test/unit/cloudinaryConfig.test.js
@@ -3,17 +3,17 @@ import VideoPlayer from '../../src/video-player';
 cloudinary.VideoPlayer = VideoPlayer;
 
 describe('secure true test', () => {
-  it('test force secure true', () => {
-    const cld = cloudinary.Cloudinary.new({ cloud_name: 'demo' });
+  it('test force secure true', async () => {
     jest.useFakeTimers();
+    const cld = cloudinary.Cloudinary.new({ cloud_name: 'demo' });
     document.body.innerHTML = '<div><video id="test"/></div>';
     let vp = new VideoPlayer('test', { hideContextMenu: true, cloudinaryConfig: cld }, false);
     let conf = vp.videojs.cloudinary.cloudinaryConfig().config();
     expect(conf.secure).toEqual(true);
   });
-  it('test explicit secure false', () => {
-    const cld = cloudinary.Cloudinary.new({ cloud_name: 'demo' });
+  it('test explicit secure false', async () => {
     jest.useFakeTimers();
+    const cld = cloudinary.Cloudinary.new({ cloud_name: 'demo' });
     document.body.innerHTML = '<div><video id="test"/></div>';
     let vp = new VideoPlayer('test', { hideContextMenu: true, cloudinaryConfig: cld, secure: false }, false);
     let conf = vp.videojs.cloudinary.cloudinaryConfig().config();
