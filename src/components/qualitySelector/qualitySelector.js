8692ff71b45c857d51d973dd935ae7ec907e20ca
Yaniv Aran-Shamir <yaniv@gambaweb.co.il>
2020-10-28 16:50:55 +0200

    change array findIndex to utils find index

--- src/components/qualitySelector/qualitySelector.js
+++ src/components/qualitySelector/qualitySelector.js
@@ -2,57 +2,10 @@ import * as djs from 'dashjs';
 import 'videojs-per-source-behaviors';
 import 'videojs-contrib-quality-levels';
 import '../../plugins/videojs-http-source-selector/plugin';
+import { findIndex } from '../../utils/find';
 
 import './quality-selector.scss';
 
-if (!Array.prototype.findIndex) {
-  // eslint-disable-next-line no-extend-native
-  Object.defineProperty(Array.prototype, 'findIndex', {
-    value: function(predicate) {
-      // 1. Let O be ? ToObject(this value).
-      if (this === null) {
-        throw new TypeError('"this" is null or not defined');
-      }
-
-      let o = Object(this);
-
-      // 2. Let len be ? ToLength(? Get(O, "length")).
-      // eslint-disable-next-line no-bitwise
-      let len = o.length >>> 0;
-
-      // 3. If IsCallable(predicate) is false, throw a TypeError exception.
-      if (typeof predicate !== 'function') {
-        throw new TypeError('predicate must be a function');
-      }
-
-      // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
-      // eslint-disable-next-line prefer-rest-params
-      let thisArg = arguments[1];
-
-      // 5. Let k be 0.
-      let k = 0;
-
-      // 6. Repeat, while k < len
-      while (k < len) {
-        // a. Let Pk be ! ToString(k).
-        // b. Let kValue be ? Get(O, Pk).
-        // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
-        // d. If testResult is true, return k.
-        let kValue = o[k];
-        if (predicate.call(thisArg, kValue, k, o)) {
-          return k;
-        }
-        // e. Increase k by 1.
-        k++;
-      }
-
-      // 7. Return -1.
-      return -1;
-    },
-    configurable: true,
-    writable: true
-  });
-}
 
 const qualitySelector = {
   init: (player) => {
@@ -114,7 +67,7 @@ const qualitySelector = {
                 if (val !== undefined) {
                   this.selected = val;
                   if (val === true) {
-                    let selectedIdx = player.qualityLevels().levels_.findIndex(l => l.id === this.id);
+                    let selectedIdx = findIndex(player.qualityLevels().levels_, (l => l.id === this.id));
                     player.qualityLevels().selectedIndex_ = selectedIdx;
                     player.qualityLevels().trigger({
                       type: 'change',
