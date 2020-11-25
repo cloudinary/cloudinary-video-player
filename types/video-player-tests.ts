499367b387b9552e63fff91f1a114ff1385481fe
Yaniv Aran-Shamir <yaniv@gambaweb.co.il>
2020-11-25 15:09:14 +0200

    side effects declerations

--- types/video-player-tests.ts
+++ types/video-player-tests.ts
@@ -2,7 +2,8 @@
  * This file is used to validate the type spec. The code is not actually run.
  */
 
-import VideoPlayer from './video-player';
+import VideoPlayer, {Options} from './video-player';
+import {Cloudinary} from 'cloudinary-core';
 
 let player: VideoPlayer = new VideoPlayer('player', {}, false);
 player.source('test', {
@@ -13,6 +14,18 @@ player.currentPublicId();
 player.pause()
 
 
+
 let player2 = new VideoPlayer('player2' , {
     bigPlayButton: false,
 }, false)
+
+const cloudinaryApi = Cloudinary.new({cloud_name: 'demo', secure: true});
+let pl = cloudinaryApi.videoPlayer('test',{bigPlayButton: true, controls: false});
+
+
+
+
+interface VideoPlayerWithVideoJs extends VideoPlayer {
+    videojs: Options;
+
+}
