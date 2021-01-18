4ee217b1152202df07fad8e8c76744e6f88382c3
Sveta Slepner <sveta.slepner@cloudinary.com>
2021-01-18 18:18:26 +0200

    fix: code refactor

--- src/plugins/cloudinary/models/video-source.js
+++ src/plugins/cloudinary/models/video-source.js
@@ -200,8 +200,8 @@ class VideoSource extends BaseSource {
 
   generateRawSource(url, type) {
     let t = type || url.split('.').pop();
-    const isAdaptive = (['mpd', 'm3u8', 'hls', 'dash'].indexOf(t) !== -1);
-    if (isAdaptive && CONTAINER_MIME_TYPES[t]) {
+    const isAdaptive = CONTAINER_MIME_TYPES[t];
+    if (isAdaptive) {
       type = CONTAINER_MIME_TYPES[t][0];
     } else {
       type = type ? `video/${type}` : null;
