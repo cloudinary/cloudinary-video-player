089a2463bac42f0c10c7cefc1bf22be1a6afc6d0
eladnoy <elad.noy@cloudinary.com>
2021-04-21 15:39:45 +0300

    moving tests there folder

--- docs/ui-config/ui-conf.test.js
+++ docs/ui-config/ui-conf.test.js
@@ -1,8 +1,11 @@
+const url = 'http://localhost:3000/ui-config/ui-config.html';
+
 describe('UI configuration tests', () => {
+
   beforeAll(async () => {
     jest.setTimeout(35000);
     await page.setViewport({width: 1280, height: 800});
-    await page.goto('http://localhost:3000/docs/ui-config.html', {waitUntil: 'load'});
+    await page.goto(url, {waitUntil: 'load'});
     await page.evaluate(() => {
       Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
         get: function() {
@@ -12,37 +15,40 @@ describe('UI configuration tests', () => {
       });
     });
   }, 10000);
+
   it('Test +/-10 sec', async () => {
     jest.setTimeout(35000);
     await page.waitForFunction('player.videojs.readyState() === 4');
     await page.waitFor(1000);
     await page.click('#player > div.vjs-control-bar > button.vjs-play-control.vjs-control.vjs-button');
-    let time = await page.$eval('#player > .vjs-control-bar > .vjs-current-time.vjs-time-control.vjs-control > .vjs-current-time-display', vt => Number(vt.textContent.replace('0:', '')));
+    const time = await page.$eval('#player > .vjs-control-bar > .vjs-current-time.vjs-time-control.vjs-control > .vjs-current-time-display', vt => Number(vt.textContent.replace('0:', '')));
     await page.click('#player > div.vjs-control-bar > button.vjs-control.vjs-icon-skip-10-plus.vjs-button');
     await page.waitFor(1000);
     await page.waitForFunction('player.videojs.readyState() === 4');
-    let newTime = await page.$eval('#player > .vjs-control-bar > .vjs-current-time.vjs-time-control.vjs-control > .vjs-current-time-display', vt => Number(vt.textContent.replace('0:', '')));
-    expect(newTime).toEqual(time + 10);
+    const newTime1 = await page.$eval('#player > .vjs-control-bar > .vjs-current-time.vjs-time-control.vjs-control > .vjs-current-time-display', vt => Number(vt.textContent.replace('0:', '')));
+    expect(newTime1).toEqual(time + 10);
     await page.click('#player > div.vjs-control-bar > button.vjs-control.vjs-icon-skip-10-min.vjs-button');
     await page.waitFor(1000);
     await page.waitForFunction('player.videojs.readyState() === 4');
-    newTime = await page.$eval('#player > .vjs-control-bar > .vjs-current-time.vjs-time-control.vjs-control > .vjs-current-time-display', vt => Number(vt.textContent.replace('0:', '')));
-    expect(newTime).toEqual(time);
+    const newTime2 = await page.$eval('#player > .vjs-control-bar > .vjs-current-time.vjs-time-control.vjs-control > .vjs-current-time-display', vt => Number(vt.textContent.replace('0:', '')));
+    expect(newTime2).toEqual(time);
   });
+
   it('Test right click no context menu', async () => {
     await page.waitForFunction('player.videojs.readyState() === 4');
     await page.waitFor(1000);
     await page.click('#player_html5_api', {button: 'right'});
     expect(await page.$('div.vjs-menu.vjs-context-menu-ui')).toEqual(null);
   });
+
   it('Test logo and logo click url', async () => {
     await page.waitForFunction('player.videojs.readyState() === 4');
     await page.waitFor(1000);
-    let opts = await page.evaluate(() => player.videojs.options_);
-    let href = await page.$eval('#player > div.vjs-control-bar > a', (a) => a.href);
-    let backImage = await page.$eval('#player > div.vjs-control-bar > a', (a) => a.style.backgroundImage);
+    const opts = await page.evaluate(() => player.videojs.options_);
+    const href = await page.$eval('#player > div.vjs-control-bar > a', (a) => a.href);
+    const backImage = await page.$eval('#player > div.vjs-control-bar > a', (a) => a.style.backgroundImage);
     expect(opts.logoOnclickUrl).toEqual(href.slice(0, href.length - 1));
-    expect(opts.logoImageUrl).toEqual(backImage.replace('url\("', ''). replace('"\)', ''));
+    expect(opts.logoImageUrl).toEqual(backImage.replace('url\("', '').replace('"\)', ''));
   });
 });
 
--- docs/ui-config/ui-config.js
+++ docs/ui-config/ui-config.js
@@ -1,8 +1,11 @@
+// needed for test;
+var player = null;
+
 window.addEventListener('load', function() {
 
   var cld = window.cloudinary.Cloudinary.new({ cloud_name: 'demo' });
 
-  cld.videoPlayer('player', {
+  player = cld.videoPlayer('player', {
     publicId: 'snow_horses',
     info: { title: 'Snow Horses', subtitle: 'A movie about horses' },
     hideContextMenu: true,
