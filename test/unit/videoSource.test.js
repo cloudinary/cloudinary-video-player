048f1bb5d22836bb0d454a31d1ba186285143b4d
eladnoy <elad.noy@cloudinary.com>
2021-04-21 15:32:10 +0300

    moving tests there folder

--- test/unit/videoSource.test.js
+++ test/unit/videoSource.test.js
@@ -3,20 +3,22 @@ import cloudinary from 'cloudinary-core';
 const cld = cloudinary.Cloudinary.new({ cloud_name: 'demo' });
 
 describe('video source tests', () => {
+
   it('Test transformation input object', () => {
-    let sourceDef = {
+    const sourceDef = {
       cloudinaryConfig: cld,
       transformation: {
         fetch_format: 'auto'
       }
     };
-    let source = new VideoSource('sea_turtle', sourceDef);
-    let srcs = source.generateSources().map(s => s.src);
+    const source = new VideoSource('sea_turtle', sourceDef);
+    const srcs = source.generateSources().map(s => s.src);
     // eslint-disable-next-line no-unused-expressions
     expect(srcs[0]).toContain('f_auto');
   });
+
   it('Test transformation input array', () => {
-    let sourceDef = {
+    const sourceDef = {
       cloudinaryConfig: cld,
       transformation: [{
         fetch_format: 'auto'
@@ -26,57 +28,64 @@ describe('video source tests', () => {
       }
       ]
     };
-    let source = new VideoSource('sea_turtle', sourceDef);
-    let srcs = source.generateSources().map(s => s.src);
+    const source = new VideoSource('sea_turtle', sourceDef);
+    const srcs = source.generateSources().map(s => s.src);
     // eslint-disable-next-line no-unused-expressions
     expect(srcs[0]).toContain('f_auto');
     expect(srcs[0]).toContain('sp_hd');
   });
+
   it('Test transformation input cloudinary transformation', () => {
-    let tr = cloudinary.Transformation.new({
+    const tr = cloudinary.Transformation.new({
       fetch_format: 'auto'
     });
-    let sourceDef = {
+    const sourceDef = {
       cloudinaryConfig: cld,
       transformation: tr
     };
-    let source = new VideoSource('sea_turtle', sourceDef);
-    let srcs = source.generateSources().map(s => s.src);
+    const source = new VideoSource('sea_turtle', sourceDef);
+    const srcs = source.generateSources().map(s => s.src);
     // eslint-disable-next-line no-unused-expressions
     expect(srcs[0]).toContain('f_auto');
   });
+
   it('Test transformation input string (named transformation)', () => {
-    let sourceDef = {
+    const sourceDef = {
       cloudinaryConfig: cld,
       transformation: 'test'
     };
-    let source = new VideoSource('sea_turtle', sourceDef);
-    let srcs = source.generateSources().map(s => s.src);
+    const source = new VideoSource('sea_turtle', sourceDef);
+    const srcs = source.generateSources().map(s => s.src);
     // eslint-disable-next-line no-unused-expressions
     expect(srcs[0]).toContain('t_test');
   });
 });
+
 describe('Adaptive source type tests', () => {
+
   it('Test hls no codec', () => {
-    let sourceDef = {
+    const sourceDef = {
       cloudinaryConfig: cld
     };
-    let source = new VideoSource('sea_turtle', sourceDef);
+
+    const source = new VideoSource('sea_turtle', sourceDef);
     source.sourceTypes(['hls']);
-    let srcs = source.generateSources().map(s => s.src);
+    const srcs = source.generateSources().map(s => s.src);
     // eslint-disable-next-line no-unused-expressions
     expect(srcs[0]).not.toContain('vc_');
   });
+
   it('Test hls with codec', () => {
-    let sourceDef = {
+    const sourceDef = {
       cloudinaryConfig: cld
     };
-    let source = new VideoSource('sea_turtle', sourceDef);
+    const source = new VideoSource('sea_turtle', sourceDef);
     source.sourceTypes(['hls/h265']);
-    let srcs = source.generateSources().map(s => s.src);
+    const srcs = source.generateSources().map(s => s.src);
     // eslint-disable-next-line no-unused-expressions
     expect(srcs[0]).toContain('vc_h265');
   });
+
   it('Test dash no codec', () => {
     let sourceDef = {
       cloudinaryConfig: cld
@@ -87,18 +96,20 @@ describe('Adaptive source type tests', () => {
     // eslint-disable-next-line no-unused-expressions
     expect(srcs[0]).not.toContain('vc_');
   });
+
   it('Test dash codec', () => {
-    let sourceDef = {
+    const sourceDef = {
       cloudinaryConfig: cld
     };
-    let source = new VideoSource('sea_turtle', sourceDef);
+    const source = new VideoSource('sea_turtle', sourceDef);
     source.sourceTypes(['dash/vp9']);
-    let srcs = source.generateSources().map(s => s.src);
+    const srcs = source.generateSources().map(s => s.src);
     // eslint-disable-next-line no-unused-expressions
     expect(srcs[0]).toContain('vc_vp9');
   });
+
   it('Test dash codec + transformation', () => {
-    let sourceDef = {
+    const sourceDef = {
       cloudinaryConfig: cld,
       transformation: [{
         fetch_format: 'auto'
@@ -108,17 +119,18 @@ describe('Adaptive source type tests', () => {
       }
       ]
     };
-    let source = new VideoSource('sea_turtle', sourceDef);
+    const source = new VideoSource('sea_turtle', sourceDef);
     source.sourceTypes(['dash/vp9']);
-    let srcs = source.generateSources().map(s => s.src);
+    const srcs = source.generateSources().map(s => s.src);
     // eslint-disable-next-line no-unused-expressions
     expect(srcs[0]).not.toContain('vc_vp9');
     expect(srcs[0]).toContain('f_auto');
     expect(srcs[0]).toContain('sp_hd');
     expect(srcs[0]).toContain('.mpd');
   });
+
   it('Test hls codec + transformation', () => {
-    let sourceDef = {
+    const sourceDef = {
       cloudinaryConfig: cld,
       transformation: [{
         fetch_format: 'auto'
@@ -128,9 +140,9 @@ describe('Adaptive source type tests', () => {
       }
       ]
     };
-    let source = new VideoSource('sea_turtle', sourceDef);
+    const source = new VideoSource('sea_turtle', sourceDef);
     source.sourceTypes(['hls/h265']);
-    let srcs = source.generateSources().map(s => s.src);
+    const srcs = source.generateSources().map(s => s.src);
     // eslint-disable-next-line no-unused-expressions
     expect(srcs[0]).not.toContain('vc_h265');
     expect(srcs[0]).toContain('f_auto');
@@ -138,136 +150,148 @@ describe('Adaptive source type tests', () => {
     expect(srcs[0]).toContain('.m3u8');
   });
 });
+
 describe('Raw url tests', () => {
+
   it('Test raw url', () => {
-    let sourceDef = {
+    const sourceDef = {
       cloudinaryConfig: cld,
       sourceTypes: ['webm']
     };
-    let url = 'https://exmaple.com/test.mp4';
-    let source = new VideoSource(url, sourceDef);
-    let srcs = source.generateSources().map(s => s.src);
+    const url = 'https://exmaple.com/test.mp4';
+    const source = new VideoSource(url, sourceDef);
+    const srcs = source.generateSources().map(s => s.src);
     expect(srcs[0]).toEqual(url);
   });
+
   it('Test raw url without type', () => {
-    let sourceDef = {
+    const sourceDef = {
       cloudinaryConfig: cld
     };
-    let url = 'https://exmaple.com/test.mp4';
-    let source = new VideoSource(url, sourceDef);
-    let srcs = source.generateSources();
+    const url = 'https://exmaple.com/test.mp4';
+    const source = new VideoSource(url, sourceDef);
+    const srcs = source.generateSources();
     expect(srcs[0].src).toEqual(url);
     expect(srcs[0].type).toEqual(null);
     expect(srcs[0].isAdaptive).toEqual(false);
   });
+
   it('Test raw url with transformations', () => {
-    let sourceDef = {
+    const sourceDef = {
       cloudinaryConfig: cld
     };
-    let url = 'https://media.castit.biz/castit-dev/video/upload/a_0,c_limit,f_mp4,vc_h264,w_600/cit-qa/Videoback/2020/08/03/269085_358976.mp4';
-    let source = new VideoSource(url, sourceDef);
-    let srcs = source.generateSources();
+    const url = 'https://media.castit.biz/castit-dev/video/upload/a_0,c_limit,f_mp4,vc_h264,w_600/cit-qa/Videoback/2020/08/03/269085_358976.mp4';
+    const source = new VideoSource(url, sourceDef);
+    const srcs = source.generateSources();
     expect(srcs[0].src).toEqual(url);
     expect(srcs[0].type).toEqual(null);
     expect(srcs[0].isAdaptive).toEqual(false);
   });
+
   it('Test raw url without extension', () => {
-    let sourceDef = {
+    const sourceDef = {
       cloudinaryConfig: cld,
       sourceTypes: ['mp4']
     };
-    let url = 'https://exmaple.com/test';
-    let source = new VideoSource(url, sourceDef);
-    let srcs = source.generateSources();
+    const url = 'https://exmaple.com/test';
+    const source = new VideoSource(url, sourceDef);
+    const srcs = source.generateSources();
     expect(srcs[0].src).toEqual(url);
     expect(srcs[0].type).toEqual('video/mp4');
   });
+
   it('Test raw url with params', () => {
-    let sourceDef = {
+    const sourceDef = {
       cloudinaryConfig: cld
     };
-    let url = 'https://exmaple.com/test.mp4?test=1234&user=eeeee&format=webm';
-    let source = new VideoSource(url, sourceDef);
+    const url = 'https://exmaple.com/test.mp4?test=1234&user=eeeee&format=webm';
+    const source = new VideoSource(url, sourceDef);
     source.sourceTypes(['webm']);
-    let srcs = source.generateSources().map(s => s.src);
+    const srcs = source.generateSources().map(s => s.src);
     expect(srcs[0]).toEqual(url);
   });
+
   it('Test raw url with anchor', () => {
-    let sourceDef = {
+    const sourceDef = {
       cloudinaryConfig: cld
     };
-    let url = 'https://exmaple.com/test.mp4#123454';
-    let source = new VideoSource(url, sourceDef);
-    source.sourceTypes(['webm']);
-    let srcs = source.generateSources().map(s => s.src);
-    expect(srcs[0]).toEqual(url);
-  });
-  it('Test raw url with anchor', () => {
-    let sourceDef = {
-      cloudinaryConfig: cld
-    };
-    let url = 'https://exmaple.com/test.mp4#123454';
-    let source = new VideoSource(url, sourceDef);
+    const url = 'https://exmaple.com/test.mp4#123454';
+    const source = new VideoSource(url, sourceDef);
     source.sourceTypes(['webm']);
     let srcs = source.generateSources().map(s => s.src);
     expect(srcs[0]).toEqual(url);
   });
 
+  it('Test raw url with anchor', () => {
+    const sourceDef = {
+      cloudinaryConfig: cld
+    };
+    const url = 'https://exmaple.com/test.mp4#123454';
+    const source = new VideoSource(url, sourceDef);
+    source.sourceTypes(['webm']);
+    const srcs = source.generateSources().map(s => s.src);
+    expect(srcs[0]).toEqual(url);
+  });
+
   it('Test raw url adaptive m3u8', () => {
-    let sourceDef = {
+    const sourceDef = {
       cloudinaryConfig: cld
     };
-    let url = 'https://exmaple.com/test.m3u8';
-    let source = new VideoSource(url, sourceDef);
-    let srcs = source.generateSources();
+    const url = 'https://exmaple.com/test.m3u8';
+    const source = new VideoSource(url, sourceDef);
+    const srcs = source.generateSources();
     expect(srcs[0].src).toEqual(url);
     expect(srcs[0].isAdaptive).toEqual(true);
     expect(srcs[0].type).toEqual('application/x-mpegURL');
   });
+
   it('Test raw url adaptive hls', () => {
-    let sourceDef = {
+    const sourceDef = {
       cloudinaryConfig: cld
     };
-    let url = 'https://exmaple.com/test';
-    let source = new VideoSource(url, sourceDef);
+    const url = 'https://exmaple.com/test';
+    const source = new VideoSource(url, sourceDef);
     source.sourceTypes(['hls']);
-    let srcs = source.generateSources();
+    const srcs = source.generateSources();
     expect(srcs[0].src).toEqual(url);
     expect(srcs[0].isAdaptive).toEqual(true);
     expect(srcs[0].type).toEqual('application/x-mpegURL');
   });
+
   it('Test raw url adaptive mpd', () => {
-    let sourceDef = {
+    const sourceDef = {
       cloudinaryConfig: cld
     };
-    let url = 'https://exmaple.com/test.mpd';
-    let source = new VideoSource(url, sourceDef);
-    let srcs = source.generateSources();
+    const url = 'https://exmaple.com/test.mpd';
+    const source = new VideoSource(url, sourceDef);
+    const srcs = source.generateSources();
     expect(srcs[0].src).toEqual(url);
     expect(srcs[0].isAdaptive).toEqual(true);
     expect(srcs[0].type).toEqual('application/dash+xml');
   });
+
   it('Test raw url adaptive dash', () => {
-    let sourceDef = {
+    const sourceDef = {
       cloudinaryConfig: cld
     };
-    let url = 'https://exmaple.com/test';
-    let source = new VideoSource(url, sourceDef);
+    const url = 'https://exmaple.com/test';
+    const source = new VideoSource(url, sourceDef);
     source.sourceTypes(['dash']);
-    let srcs = source.generateSources();
+    const srcs = source.generateSources();
     expect(srcs[0].src).toEqual(url);
     expect(srcs[0].isAdaptive).toEqual(true);
     expect(srcs[0].type).toEqual('application/dash+xml');
   });
+
   it('Should not break when calling generateSources() more then once', () => {
-    let sourceDef = {
+    const sourceDef = {
       cloudinaryConfig: cld
     };
-    let url = 'https://exmaple.com/test';
-    let source = new VideoSource(url, sourceDef);
+    const url = 'https://exmaple.com/test';
+    const source = new VideoSource(url, sourceDef);
     source.sourceTypes(['hls']);
     source.generateSources();
-    let srcs = source.generateSources();
+    const srcs = source.generateSources();
     expect(srcs[0].src).toEqual(url);
     expect(srcs[0].isAdaptive).toEqual(true);
     expect(srcs[0].type).toEqual('application/x-mpegURL');
