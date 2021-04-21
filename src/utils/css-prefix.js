ec22570b7fb7e80b39300b39091f8226076c4757
eladnoy <elad.noy@cloudinary.com>
2021-04-21 08:42:55 +0300

    fix: APPS-9253 lintfix

--- src/utils/css-prefix.js
+++ src/utils/css-prefix.js
@@ -9,9 +9,7 @@ const playerClassPrefix = (componentInstance) => `${CLASS_PREFIX}-${componentIns
 
 const skinClass = (skin) => `${SKIN_CLASS_PREFIX}${skin}`;
 
-const skinClassPrefix = (componentInstance) => {
-  return find(componentInstance.el().classList, (cls) => startsWith(cls, SKIN_CLASS_PREFIX));
-};
+const skinClassPrefix = (componentInstance) => find(componentInstance.el().classList, (cls) => startsWith(cls, SKIN_CLASS_PREFIX));
 
 const setSkinClassPrefix = (componentInstance, name) => {
   const currentSkinPrefix = skinClassPrefix(componentInstance);
