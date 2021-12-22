22e08b7f700d731dc148f16c49e73014cda626d2
Tsachi Shlidor <tsachi.shlidor@cloudinary.com>
2021-12-22 17:01:02 +0200

    upgrade dash & videojs-dash

--- src/plugins/dash/videojs-dash.js
+++ src/plugins/dash/videojs-dash.js
@@ -4,14 +4,12 @@ import dashjs from 'dashjs';
 import setupAudioTracks from './setup-audio-tracks';
 import setupTextTracks from './setup-text-tracks';
 import document from 'global/document';
-import { assign } from '../../utils/assign';
 
 /**
  * videojs-contrib-dash
  *
  * Use Dash.js to playback DASH content inside of Video.js via a SourceHandler
  */
-
 class Html5DashJS {
   constructor(source, tech, options) {
     // Get options from tech if not provided for backwards compatibility
@@ -55,11 +53,6 @@ class Html5DashJS {
 
     this.mediaPlayer_ = this.player.dash.mediaPlayer;
 
-    // For whatever reason, we need to call setTextDefaultEnabled(false) to get
-    // VTT captions to show, even though we're doing virtually the same thing
-    // in setup-text-tracks.js
-    this.mediaPlayer_.setTextDefaultEnabled(false);
-
     // Log MedaPlayer messages through video.js
     if (Html5DashJS.useVideoJSDebug) {
       videojs.log.warn('useVideoJSDebug has been deprecated.' +
@@ -94,20 +87,92 @@ class Html5DashJS {
     // - mediaKeyMessageError (only fires under 'might not work' circumstances)
     // eslint-disable-next-line complexity
     this.retriggerError_ = (event) => {
-      if (event.type === 'error') {
+      if (event.error === 'capability' && event.event === 'mediasource') {
+        // No support for MSE
+        this.player.error({
+          code: 4,
+          message: 'The media cannot be played because it requires a feature ' +
+            'that your browser does not support.'
+        });
 
-        /* initialize errros for ref https://cdn.dashjs.org/latest/jsdoc/core_errors_Errors.js.html
-          map to general init error
-        */
-        if (event.error.code >= 10 && event.error.code <= 35) {
-          this.player.error({ code: 4, dashjsErrorCode: event.error.code, message: event.error.message });
-        } else if (event.error.code < 10) { // network errors
-          this.player.error({ code: event.error.code, dashjsErrorCode: event.error.code, message: event.error.message });
-        } else if (event.error.code >= 100 && event.error.code <= 114) { // Protection Errors  https://cdn.dashjs.org/latest/jsdoc/streaming_protection_errors_ProtectionErrors.js.html
-          this.player.error({ code: 5, dashjsErrorCode: event.error.code, message: event.error.message });
+      } else if (event.error === 'manifestError' && (
+        // Manifest type not supported
+        (event.event.id === 'createParser') ||
+        // Codec(s) not supported
+        (event.event.id === 'codec') ||
+        // No streams available to stream
+        (event.event.id === 'nostreams') ||
+        // Error creating Stream object
+        (event.event.id === 'nostreamscomposed') ||
+        // syntax error parsing the manifest
+        (event.event.id === 'parse') ||
+        // a stream has multiplexed audio+video
+        (event.event.id === 'multiplexedrep')
+      )) {
+        // These errors have useful error messages, so we forward it on
+        this.player.error({ code: 4, message: event.event.message });
+
+      } else if (event.error === 'mediasource') {
+        // This error happens when dash.js fails to allocate a SourceBuffer
+        // OR the underlying video element throws a `MediaError`.
+        // If it's a buffer allocation fail, the message states which buffer
+        // (audio/video/text) failed allocation.
+        // If it's a `MediaError`, dash.js inspects the error object for
+        // additional information to append to the error type.
+        if (event.event.match('MEDIA_ERR_ABORTED')) {
+          this.player.error({ code: 1, message: event.event });
+        } else if (event.event.match('MEDIA_ERR_NETWORK')) {
+          this.player.error({ code: 2, message: event.event });
+        } else if (event.event.match('MEDIA_ERR_DECODE')) {
+          this.player.error({ code: 3, message: event.event });
+        } else if (event.event.match('MEDIA_ERR_SRC_NOT_SUPPORTED')) {
+          this.player.error({ code: 4, message: event.event });
+        } else if (event.event.match('MEDIA_ERR_ENCRYPTED')) {
+          this.player.error({ code: 5, message: event.event });
+        } else if (event.event.match('UNKNOWN')) {
+          // We shouldn't ever end up here, since this would mean a
+          // `MediaError` thrown by the video element that doesn't comply
+          // with the W3C spec. But, since we should handle the error,
+          // throwing a MEDIA_ERR_SRC_NOT_SUPPORTED is probably the
+          // most reasonable thing to do.
+          this.player.error({ code: 4, message: event.event });
         } else {
-          this.player.error({ code: event.error.code, message: event.error.message });
+          // Buffer allocation error
+          this.player.error({ code: 4, message: event.event });
         }
+
+      } else if (event.error === 'capability' && event.event === 'encryptedmedia') {
+        // Browser doesn't support EME
+        this.player.error({
+          code: 5,
+          message: 'The media cannot be played because it requires encryption ' +
+            'features that your browser does not support.'
+        });
+
+      } else if (event.error === 'key_session') {
+        // This block handles pretty much all errors thrown by the
+        // encryption subsystem
+        this.player.error({
+          code: 5,
+          message: event.event
+        });
+
+      } else if (event.error === 'download') {
+        this.player.error({
+          code: 2,
+          message: 'The media playback was aborted because too many consecutive ' +
+            'download errors occurred.'
+        });
+
+      } else if (event.error === 'mssError') {
+        this.player.error({
+          code: 3,
+          message: event.event
+        });
+
+      } else {
+        // ignore the error
+        return;
       }
 
       // only reset the dash player in 10ms async, so that the rest of the
@@ -141,6 +206,10 @@ class Html5DashJS {
     // Apply all dash options that are set
     if (options.dash) {
       Object.keys(options.dash).forEach((key) => {
+        if (key === 'useTTML') {
+          return;
+        }
+
         const dashOptionsKey = 'set' + key.charAt(0).toUpperCase() + key.slice(1);
         let value = options.dash[key];
 
@@ -174,13 +243,18 @@ class Html5DashJS {
 
     this.mediaPlayer_.attachView(this.el_);
 
+    if (options.dash && options.dash.useTTML) {
+      this.ttmlContainer_ = this.player.addChild('TTMLTextTrackDisplay');
+      this.mediaPlayer_.attachTTMLRenderingDiv(this.ttmlContainer_.el());
+    }
+
     // Dash.js autoplays by default, video.js will handle autoplay
     this.mediaPlayer_.setAutoPlay(false);
 
     // Setup audio tracks
     // eslint-disable-next-line no-useless-call
     setupAudioTracks.call(null, this.player, tech);
-
+    
     // Setup text tracks
     // eslint-disable-next-line no-useless-call
     setupTextTracks.call(null, this.player, tech, options);
@@ -188,11 +262,8 @@ class Html5DashJS {
     // Attach the source with any protection data
     this.mediaPlayer_.setProtectionData(this.keySystemOptions_);
     this.mediaPlayer_.attachSource(manifestSource);
+
     this.tech_.triggerReady();
-    // map videojs seek
-    this.player.on(this.tech_, 'seeking', () => {
-      this.mediaPlayer_.seek(this.tech_.currentTime());
-    });
   }
 
   /*
@@ -233,6 +304,11 @@ class Html5DashJS {
     if (this.player.dash) {
       delete this.player.dash;
     }
+
+    if (this.ttmlContainer_) {
+      this.ttmlContainer_.dispose();
+      this.player.removeChild('TTMLTextTrackDisplay');
+    }
   }
 
   duration() {
@@ -247,7 +323,7 @@ class Html5DashJS {
    * Get a list of hooks for a specific lifecycle
    *
    * @param {string} type the lifecycle to get hooks from
-   * @param {Function=|Function[]=} hook Optionally add a hook tothe lifecycle
+   * @param {Function|Function[]} [hook] Optionally add a hook tothe lifecycle
    * @return {Array} an array of hooks or epty if none
    * @method hooks
    */
