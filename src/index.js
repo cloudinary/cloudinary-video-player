a788685f41158c6c5f2d110f8a1488a9157d9f01
eladnoy <elad.noy@cloudinary.com>
2022-03-03 12:42:07 +0200

    omit playerOptions

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import 'assets/styles/main.scss';
 import VideoPlayer from './video-player';
 import { assign } from 'utils/assign';
-import { pick } from './utils/object';
+import { omit, pick } from './utils/object';
 import { CLOUDINARY_CONFIG_PARAM } from './video-player.const';
 
 if (window.cloudinary && window.cloudinary.Cloudinary) {
   console.warn('For version 1.9.0 and above, cloudinary-core is not needed for using the Cloudinary Video Player');
 }
 
-const getConfig = (playerOptions = {}, cloudinaryConfig) => assign(playerOptions, { cloudinaryConfig: cloudinaryConfig || pick(playerOptions, CLOUDINARY_CONFIG_PARAM) });
+const getConfig = (playerOptions = {}, cloudinaryConfig) => assign(
+  omit(playerOptions, CLOUDINARY_CONFIG_PARAM),
+  { cloudinaryConfig: cloudinaryConfig || pick(playerOptions, CLOUDINARY_CONFIG_PARAM) });
 
 const getVideoPlayer = (config) => (id, playerOptions, ready) => new VideoPlayer(id, getConfig(playerOptions, config), ready);
 
