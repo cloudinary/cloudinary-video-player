556bf1bcdc6670f16c6fa1e8edca905a4dc3e317
Tsachi Shlidor <tsachi.shlidor@cloudinary.com>
2023-07-09 13:11:43 +0300

    update bundle

--- src/index.all.js
+++ src/index.all.js
@@ -1,5 +1,9 @@
+// This file is bundled as `cld-video-player.js` to be imported as a single module that
+// includes all plugins.
+
 // Usage:
 // import { videoPlayer, videoPlayers } from 'cloudinary-video-player';
 
 export * from './index.js';
 export * from './plugins/dash/videojs-dash.js';
+export * from './plugins/ima/index.js';
