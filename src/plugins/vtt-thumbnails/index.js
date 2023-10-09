3edf874a70524390199851bcb4eb3d5c3b26177d
Tsachi Shlidor <tsachi.shlidor@cloudinary.com>
2023-10-09 21:48:18 +0300

    fix: handle case of no seek-thumbnails

--- src/plugins/vtt-thumbnails/index.js
+++ src/plugins/vtt-thumbnails/index.js
@@ -145,10 +145,6 @@ const VttThumbnailsPlugin = (function () {
         }
       });
       this.getChild('timeTooltip').write(videojs.time.formatTime(time));
-
-      this.getChild('timeTooltip').update = function() {
-        this.el_.style.transform = 'translateX(-50%)';
-      };
     };
   };
 
