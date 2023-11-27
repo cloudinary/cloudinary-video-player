7b8f3ac5ae94345b5ab5bdfa5b8daa23e7708e37
Tsachi Shlidor <tsachi.shlidor@cloudinary.com>
2023-11-27 18:12:05 +0200

    fix: add missing configurations

--- src/utils/get-analytics-player-options.js
+++ src/utils/get-analytics-player-options.js
@@ -1,48 +1,80 @@
+import defaults from 'config/defaults';
+
+const stringify = (obj) => new URLSearchParams(obj).toString();
+
+const filterDefaultsAndNulls = (obj) => Object.entries(obj).reduce((filtered, [key, value]) => {
+  if (value !== defaults[key] && value !== undefined && value !== null) {
+    filtered[key] = value;
+  }
+  return filtered;
+}, {});
+
 const getCloudinaryOptions = (cloudinaryOptions = {}) => ({
-  sourceTypes: cloudinaryOptions.sourceTypes || null,
-  autoShowRecommendations: cloudinaryOptions.autoShowRecommendations || null,
-  fontFace: cloudinaryOptions.fontFace || null,
+  sourceTypes: cloudinaryOptions.sourceTypes,
+  autoShowRecommendations: cloudinaryOptions.autoShowRecommendations,
+  fontFace: cloudinaryOptions.fontFace,
+  posterOptions: !!cloudinaryOptions.posterOptions,
   posterOptionsPublicId: !!(cloudinaryOptions.posterOptions && cloudinaryOptions.posterOptions.publicId)
 });
 
 const getSourceOptions = (sourceOptions = {}) => ({
-  shoppable: !!sourceOptions.shoppable,
-  shoppableProductsLength: sourceOptions.shoppable && Array.isArray(sourceOptions.shoppable.products) ? sourceOptions.shoppable.products.length : 0,
+  chapters: sourceOptions.chapters && (sourceOptions.chapters.url ? 'url' : JSON.stringify(sourceOptions.chapters)),
+  recommendations: sourceOptions.recommendations && sourceOptions.recommendations.length,
+  shoppable: sourceOptions.shoppable && !!sourceOptions.shoppable,
+  shoppableProductsLength: sourceOptions.shoppable && sourceOptions.shoppable.products && sourceOptions.shoppable.products.length,
   ...(sourceOptions.info ? {
-    sourceInfoTitle: sourceOptions.info.title || null,
-    sourceInfoSubtitle: sourceOptions.info.subtitle || null,
-    sourceInfoDescription: sourceOptions.info.description || null
-  } : {})
+    sourceInfoTitle: sourceOptions.info.title,
+    sourceInfoSubtitle: sourceOptions.info.subtitle,
+    sourceInfoDescription: sourceOptions.info.description
+  } : {}),
+  textTracks: sourceOptions.textTracks && !!sourceOptions.textTracks
 });
 
 const getAdsOptions = (adsOptions = {}) => ({
-  adsAdTagUrl: adsOptions.adTagUrl || null,
-  adsShowCountdown: adsOptions.showCountdown || null,
-  adsAdLabel: adsOptions.adLabel || null,
-  adsLocale: adsOptions.locale || null,
-  adsPrerollTimeout: adsOptions.prerollTimeout || null,
-  adsPostrollTimeout: adsOptions.postrollTimeout || null,
-  adsAdsInPlaylist: adsOptions.adsInPlaylist || null
+  adsAdTagUrl: adsOptions.adTagUrl,
+  adsShowCountdown: adsOptions.showCountdown,
+  adsAdLabel: adsOptions.adLabel,
+  adsLocale: adsOptions.locale,
+  adsPrerollTimeout: adsOptions.prerollTimeout,
+  adsPostrollTimeout: adsOptions.postrollTimeout,
+  adsAdsInPlaylist: adsOptions.adsInPlaylist
 });
 
 const getPlaylistWidgetOptions = (playlistWidgetOptions = {}) => ({
-  playlistWidgetDirection: playlistWidgetOptions.direction || null,
-  playlistWidgetTotal: playlistWidgetOptions.total || null
+  playlistWidgetDirection: playlistWidgetOptions.direction,
+  playlistWidgetTotal: playlistWidgetOptions.total
 });
 
-export const getAnalyticsFromPlayerOptions = (playerOptions) => {
-  return {
-    showJumpControls: playerOptions.showJumpControls || null,
-    seekThumbnails: playerOptions.seekThumbnails || null,
-    aiHighlightsGraph: playerOptions.aiHighlightsGraph || null,
-    floatingWhenNotVisible: playerOptions.floatingWhenNotVisible || null,
-    hideContextMenu: playerOptions.hideContextMenu || null,
-    analytics: playerOptions.analytics || null,
-    cloudinaryAnalytics: playerOptions.cloudinaryAnalytics || null,
-    playedEventPercents: playerOptions.playedEventPercents || null,
-    ...getCloudinaryOptions(playerOptions.cloudinary),
-    ...getSourceOptions(playerOptions.source),
-    ...getAdsOptions(playerOptions.ads),
-    ...getPlaylistWidgetOptions(playerOptions.playlistWidget)
-  };
-};
+export const getAnalyticsFromPlayerOptions = (playerOptions) => filterDefaultsAndNulls({
+  aiHighlightsGraph: playerOptions.aiHighlightsGraph,
+  analytics: playerOptions.analytics,
+  autoplay: playerOptions.autoplay,
+  autoplayMode: playerOptions.autoplayMode,
+  className: playerOptions.class,
+  cloudinaryAnalytics: playerOptions.cloudinaryAnalytics,
+  controls: playerOptions.controls,
+  floatingWhenNotVisible: playerOptions.floatingWhenNotVisible,
+  fluid: playerOptions.fluid,
+  height: playerOptions.height,
+  hideContextMenu: playerOptions.hideContextMenu,
+  logoImageUrl: playerOptions.logoImageUrl,
+  logoOnclickUrl: playerOptions.logoOnclickUrl,
+  loop: playerOptions.loop,
+  muted: playerOptions.muted,
+  playedEventPercents: playerOptions.playedEventPercents,
+  playsinline: playerOptions.playsinline,
+  preload: playerOptions.preload,
+  seekThumbnails: playerOptions.seekThumbnails,
+  showJumpControls: playerOptions.showJumpControls,
+  showLogo: playerOptions.showLogo,
+  skin: playerOptions.skin,
+  width: playerOptions.width,
+
+  colors: playerOptions.colors && stringify(playerOptions.colors),
+  controlBar: (JSON.stringify(playerOptions.controlBar) !== JSON.stringify(defaults.controlBar)) ? stringify(playerOptions.controlBar) : null,
+
+  ...getCloudinaryOptions(playerOptions.cloudinary),
+  ...getSourceOptions(playerOptions.source),
+  ...getAdsOptions(playerOptions.ads),
+  ...getPlaylistWidgetOptions(playerOptions.playlistWidget)
+});
