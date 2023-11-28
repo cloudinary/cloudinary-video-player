14947d22f3f7c638c67788991881f9950648008b
Tsachi Shlidor <tsachi.shlidor@cloudinary.com>
2023-11-28 12:32:52 +0200

    fix: hasConfig

--- src/utils/get-analytics-player-options.js
+++ src/utils/get-analytics-player-options.js
@@ -1,6 +1,9 @@
 import defaults from 'config/defaults';
+import isEmpty from 'lodash/isEmpty';
 
 const stringify = (obj) => new URLSearchParams(obj).toString();
+
+const hasConfig = (obj) => isEmpty(obj) ? null : true;
 
 const filterDefaultsAndNulls = (obj) => Object.entries(obj).reduce((filtered, [key, value]) => {
   if (value !== defaults[key] && value !== undefined && value !== null) {
@@ -13,21 +16,21 @@ const getCloudinaryOptions = (cloudinaryOptions = {}) => ({
   sourceTypes: cloudinaryOptions.sourceTypes,
   autoShowRecommendations: cloudinaryOptions.autoShowRecommendations,
   fontFace: cloudinaryOptions.fontFace,
-  posterOptions: !!cloudinaryOptions.posterOptions,
-  posterOptionsPublicId: !!(cloudinaryOptions.posterOptions && cloudinaryOptions.posterOptions.publicId)
+  posterOptions: hasConfig(cloudinaryOptions.posterOptions),
+  posterOptionsPublicId: cloudinaryOptions.posterOptions && hasConfig(cloudinaryOptions.posterOptions.publicId)
 });
 
 const getSourceOptions = (sourceOptions = {}) => ({
   chapters: sourceOptions.chapters && (sourceOptions.chapters.url ? 'url' : JSON.stringify(sourceOptions.chapters)),
   recommendations: sourceOptions.recommendations && sourceOptions.recommendations.length,
-  shoppable: sourceOptions.shoppable && !!sourceOptions.shoppable,
+  shoppable: hasConfig(sourceOptions.shoppable),
   shoppableProductsLength: sourceOptions.shoppable && sourceOptions.shoppable.products && sourceOptions.shoppable.products.length,
   ...(sourceOptions.info ? {
     sourceInfoTitle: sourceOptions.info.title,
     sourceInfoSubtitle: sourceOptions.info.subtitle,
     sourceInfoDescription: sourceOptions.info.description
   } : {}),
-  textTracks: sourceOptions.textTracks && !!sourceOptions.textTracks
+  textTracks: hasConfig(sourceOptions.textTracks)
 });
 
 const getAdsOptions = (adsOptions = {}) => ({
@@ -50,6 +53,7 @@ export const getAnalyticsFromPlayerOptions = (playerOptions) => filterDefaultsAn
   analytics: playerOptions.analytics,
   autoplay: playerOptions.autoplay,
   autoplayMode: playerOptions.autoplayMode,
+  bigPlayButton: playerOptions.bigPlayButton,
   className: playerOptions.class,
   cloudinaryAnalytics: playerOptions.cloudinaryAnalytics,
   controls: playerOptions.controls,
@@ -60,15 +64,21 @@ export const getAnalyticsFromPlayerOptions = (playerOptions) => filterDefaultsAn
   logoImageUrl: playerOptions.logoImageUrl,
   logoOnclickUrl: playerOptions.logoOnclickUrl,
   loop: playerOptions.loop,
+  maxTries: playerOptions.maxTries,
   muted: playerOptions.muted,
+  playbackRates: playerOptions.playbackRates,
   playedEventPercents: playerOptions.playedEventPercents,
+  playedEventTimes: playerOptions.playedEventTimes,
   playsinline: playerOptions.playsinline,
   preload: playerOptions.preload,
+  videoTimeout: playerOptions.videoTimeout,
   seekThumbnails: playerOptions.seekThumbnails,
   showJumpControls: playerOptions.showJumpControls,
   showLogo: playerOptions.showLogo,
   skin: playerOptions.skin,
+  videoJS: hasConfig(playerOptions.videoJS),
   width: playerOptions.width,
+  withCredentials: playerOptions.withCredentials,
 
   colors: playerOptions.colors && stringify(playerOptions.colors),
   controlBar: (JSON.stringify(playerOptions.controlBar) !== JSON.stringify(defaults.controlBar)) ? stringify(playerOptions.controlBar) : null,
