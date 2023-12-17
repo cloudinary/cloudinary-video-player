582cc08017bc4b66030b499e444ca943079fabe3
Tsachi Shlidor <tsachi.shlidor@cloudinary.com>
2023-12-17 12:06:02 +0200

    fix: es-imports

--- src/plugins/styled-text-tracks/index.js
+++ src/plugins/styled-text-tracks/index.js
@@ -1,7 +1,7 @@
 export default async function lazyPlugin(config) {
   const player = this;
   try {
-    const { default: initPlugin } = await import('./styled-text-tracks');
+    const { default: initPlugin } = await import(/* webpackChunkName: "styled-text-tracks" */ './styled-text-tracks');
     player.ready(() => initPlugin(config, player));
   } catch (error) {
     console.error('Failed to load plugin:', error);
