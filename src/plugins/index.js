805797a9117461b129c902d1315b6f0f6891ad65
Tsachi Shlidor <tsachi.shlidor@cloudinary.com>
2023-12-28 14:00:47 +0200

    fix: Update index.js

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -23,6 +23,11 @@ import vttThumbnails from './vtt-thumbnails';
 import styledTextTracks from './styled-text-tracks';
 
 const plugins = {
+  // #if (!process.env.WEBPACK_BUILD_LIGHT)
+  dashPlugin,
+  interactive,
+  imaPlugin
+  // #endif
   aiHighlightsGraph,
   analytics,
   autoplayOnScroll,
@@ -35,11 +40,6 @@ const plugins = {
   pacedTranscript,
   styledTextTracks,
   vttThumbnails,
-  // #if (!process.env.WEBPACK_BUILD_LIGHT)
-  interactive,
-  dashPlugin,
-  imaPlugin
-  // #endif
 };
 
 export default plugins;
