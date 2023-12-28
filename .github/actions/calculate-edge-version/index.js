fa9fae664d43e3db555927d6109a6fb08918c4c4
Tsachi Shlidor <tsachi.shlidor@cloudinary.com>
2023-12-28 15:52:43 +0200

    ci: versioning

--- .github/actions/calculate-edge-version/index.js
+++ .github/actions/calculate-edge-version/index.js
@@ -3,6 +3,12 @@ const semver = require('semver');
 
 try {
   const packageVersion = core.getInput('version', { require: true });
+
+  // Remove initial v if present
+  if (packageVersion.charAt(0) === 'v') {
+    packageVersion = packageVersion.substring(1);
+  }
+
   core.setOutput('next-edge', semver.inc(packageVersion, 'prerelease', undefined, 'edge'));
 } catch (error) {
   core.setFailed(`Action failed to transform input with error: ${error.message}`);
