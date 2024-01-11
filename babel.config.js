0d7b818919cc9ef64c87e720c76ea110e0c78611
Tsachi Shlidor <tsachi.shlidor@cloudinary.com>
2024-01-11 16:41:13 +0200

    fix: babel config

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,14 @@
 module.exports = {
-  presets: ['@babel/preset-env'],
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          browsers: ['>0.25%', 'not ie 11', 'not op_mini all']
+        }
+      }
+    ]
+  ],
   env: {
     test: {
       plugins: ['@babel/plugin-transform-runtime']
