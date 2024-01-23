bad3e3271512fe3a40b470646ba4dd75581520fc
Tsachi Shlidor <tsachi.shlidor@cloudinary.com>
2024-01-23 10:06:46 +0200

    chore: lazyShoppablePlugin

--- src/plugins/shoppable-plugin/index.js
+++ src/plugins/shoppable-plugin/index.js
@@ -1,4 +1,4 @@
-export default async function lazyPlugin(player, options) {
+export default async function lazyShoppablePlugin(player, options) {
 
   const { default: ShoppableWidget } = await import(/* webpackChunkName: "shoppable" */ '../../components/shoppable-bar/shoppable-widget');
   new ShoppableWidget(player, options.shoppable).init();
