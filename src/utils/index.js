52f378b967579693c4fc647f80902ffaa51a3c5a
Tsachi Shlidor <tsachi.shlidor@cloudinary.com>
2024-03-26 17:30:10 +0200

    perf: utils refactor (groupBy, matches)

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,7 +2,6 @@ import * as slicing from './slicing';
 import * as positioning from './positioning';
 import * as string from './string';
 import * as cloudinaryUtils from './cloudinary';
-import * as groupBy from './groupBy';
 import * as mixin from './mixin';
 import * as fontFace from './fontFace';
 import * as playButton from './playButton';
@@ -16,7 +15,6 @@ const Utils = Object.assign({},
   cloudinaryUtils,
   fontFace,
   playButton,
-  groupBy,
   mixin,
   cssPrefix,
   normalizeAttributes
