575bca2a05a7d637485a83c6a02aac93995cd2c9
shaylevi <shay.levi@cloudinary.com>
2024-05-16 12:40:28 +0300

    vp test: initial first test

--- test/e2e/specs/example.spec.ts
+++ test/e2e/specs/example.spec.ts
@@ -1,15 +1,26 @@
 import { expect } from '@playwright/test';
 import { vpTest } from '../fixtures/vpTest';
 
-
+const skipLinksArray = ['ESM Imports', 'Custom Errors', 'Debug mode'];
 
 vpTest('Check no errors on console', async ({ page, consoleErrors, vpExamples }) => {
-  await vpExamples.clickLinkByName('Adaptive Streaming');
-  const a = await vpExamples.examplesList.all();
-  for (const li of a) {
-    const linkAttribute = await li.getAttribute('href');
-    await li.click();
-    expect((page.url()).includes(linkAttribute)).toBeTruthy();
-    expect(consoleErrors).toHaveLength(0);
+  //await vpExamples.clickLinkByName('Adaptive Streaming');
+  const listItems = await vpExamples.examplesList.getByRole('listitem').all();
+  for (const listItem of listItems) {
+    const link = await listItem.getByRole('link');
+    const linkAttribute = await link.getAttribute('href');
+    const linkText = await link.innerText();
+    if (skipLinksArray.includes(linkText)){
+      continue;
+    }
+    const linkAttributeModified = linkAttribute.slice(1);
+    await link.click();
+    await page.waitForLoadState('load');
+    await vpTest.step(`Navigate to link ${linkAttributeModified}`, async ()=> {
+      expect(page.url()).toContain(linkAttributeModified);
+      expect(consoleErrors).toHaveLength(0);
+      consoleErrors = [];
+      await vpExamples.goto();
+    });
   }
 });
--- test/e2e/src/pom/videoPlayerExamplesPage.ts
+++ test/e2e/src/pom/videoPlayerExamplesPage.ts
@@ -1,19 +1,19 @@
 import { Locator, Page } from '@playwright/test';
 
-const EXAMPLE_LINKS_LIST_SELECTOR = "//*[@data-testid='examples-link']//li";
+const EXAMPLE_LINKS_LIST_ID = "examples-link";
 export class VideoPlayerExamplesPage {
   private page: Page;
   public examplesList: Locator;
   constructor(page: Page) {
     this.page= page;
-    this.examplesList= page.locator(EXAMPLE_LINKS_LIST_SELECTOR);
+    this.examplesList= page.getByTestId(EXAMPLE_LINKS_LIST_ID);
   }
   public async clickLinkByName(name: string): Promise<void> {
     await this.page.getByRole('link', { name }).click();
     await this.page.waitForLoadState('networkidle');
   }
   public async goto(): Promise<void> {
-    await this.page.goto('http://localhost:3000/index.html')
+    await this.page.goto('http://localhost:3000/index.html');
   }
 }
 
