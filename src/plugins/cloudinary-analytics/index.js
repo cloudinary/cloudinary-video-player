4dbb73a06919bba02923d20353f22fb8decbb419
jakubroch <jakub.roch@cloudinary.com>
2024-06-04 06:04:09 +0200

    fix: use videojs events & re-trigger to analytics package with custom events

--- src/plugins/cloudinary-analytics/index.js
+++ src/plugins/cloudinary-analytics/index.js
@@ -1,10 +1,14 @@
-import { connectCloudinaryAnalytics } from 'cloudinary-video-analytics';
+import videojs from 'video.js';
+import { connectCloudinaryAnalytics } from '../../../../cloudinary-video-analytics';
 import { PLAYER_EVENT } from '../../utils/consts';
 
 class CloudinaryAnalytics {
   constructor(player) {
     this.player = player;
-    this.cloudinaryAnalytics = connectCloudinaryAnalytics(this.player.videoElement);
+    this.shouldUseCustomEvents = videojs.browser.IS_IOS;
+    this.cloudinaryAnalytics = connectCloudinaryAnalytics(this.player.videoElement, {
+      customEvents: this.shouldUseCustomEvents,
+    });
     this.currentVideMetadata = {
       cloudName: null,
       publicId: null
@@ -29,7 +33,31 @@ class CloudinaryAnalytics {
     }
   };
 
+  dispatchCustomEventOnVideoPlayer = (name, detail = {}) => {
+    const ev = new CustomEvent(`cld-custom-${name}`, {
+      detail,
+    });
+    this.player.videoElement.dispatchEvent(ev);
+  };
+
+  connectCustomEvents = () => {
+    this.player.on(PLAYER_EVENT.PLAY, () => this.dispatchCustomEventOnVideoPlayer('play'));
+    this.player.on(PLAYER_EVENT.PAUSE, () => this.dispatchCustomEventOnVideoPlayer('pause'));
+    this.player.on(PLAYER_EVENT.EMPTIED, () => this.dispatchCustomEventOnVideoPlayer('emptied'));
+    this.player.on(PLAYER_EVENT.LOADED_METADATA, () => {
+      const videoDuration = this.player.videoElement.duration || null;
+      this.dispatchCustomEventOnVideoPlayer('loadedmetadata', {
+        videoDuration,
+      });
+    });
+  };
+
   init() {
+    // only for iOS there is problem with reporting events because videojs re-triggers events and stops native ones
+    if (this.shouldUseCustomEvents) {
+      this.connectCustomEvents();
+    }
+
     this.player.on(PLAYER_EVENT.CLD_SOURCE_CHANGED, this.sourceChanged);
   }
 }
