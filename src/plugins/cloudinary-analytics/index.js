fb9c163b3ee1e5a1d8f54c82a27fcf9bd8fd9219
jakubroch <jakub.roch@cloudinary.com>
2024-06-10 08:56:13 +0200

    fix: import / bump package

--- src/plugins/cloudinary-analytics/index.js
+++ src/plugins/cloudinary-analytics/index.js
@@ -1,5 +1,5 @@
 import videojs from 'video.js';
-import { connectCloudinaryAnalytics } from '../../../../cloudinary-video-analytics';
+import { connectCloudinaryAnalytics } from 'cloudinary-video-analytics';
 import { PLAYER_EVENT } from '../../utils/consts';
 
 class CloudinaryAnalytics {
