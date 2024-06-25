62564f2e91d1a1dbce3760645fe55a78192a989a
shaylevi <shay.levi@cloudinary.com>
2024-06-25 14:31:52 +0300

    vp test: change back to for and update number of workers

--- test/e2e/playwright.config.ts
+++ test/e2e/playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
     /* Retry on CI only */
     retries: process.env.CI ? 2 : 0,
     /* Opt out of parallel tests on CI. */
-    workers: process.env.CI ? 1 : 6,
+    workers: 6,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: [['list'], ['html', { open: 'never' }]],
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
