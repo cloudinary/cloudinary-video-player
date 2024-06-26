42207bb8f1310b84dcf02fca57b316d4150b119f
Tsachi Shlidor <tsachi.shlidor@cloudinary.com>
2024-06-26 15:10:29 +0300

    fix: fetchLatestRelease

--- .github/actions/fetch-latest-release/index.js
+++ .github/actions/fetch-latest-release/index.js
@@ -2,42 +2,32 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const process = require('process');
 
-async function findMostRecentRelease(owner, repo) {
-  const octokit = github.getOctokit(core.getInput('github_token', { required: true }));
-
-  const releases = await octokit.rest.repos.listReleases({
-    owner: owner,
-    repo: repo
-  });
-
-  // Filter out draft releases
-  const nonDraftReleases = releases.data.filter(release => !release.draft);
-
-  // Sort releases by creation date (you can also sort by published date if preferred)
-  const sortedReleases = nonDraftReleases.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
-
-  // Get the most recent release, including prereleases but excluding drafts
-  const latestRelease = sortedReleases[0];
-
-  return latestRelease;
-}
-
 async function fetchLatestRelease() {
   const [owner, repo] = process.env['GITHUB_REPOSITORY'].split('/', 2);
+
+  const octokit = github.getOctokit(core.getInput('github_token', { required: true }));
 
   core.info(`Fetching the latest release for \`${owner}/${repo}\``);
 
   let latestRelease;
 
   try {
-    latestRelease = await findMostRecentRelease({
-      owner,
-      repo
+    const releases = await octokit.rest.repos.listReleases({
+      owner: owner,
+      repo: repo
     });
+
+    const nonDraftReleases = releases.data.filter(release => !release.draft);
+
+    const sortedReleases = nonDraftReleases.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+
+    latestRelease = sortedReleases[0];
   } catch (error) {
     core.info('Could not fetch the latest release. Have you made one yet?');
     core.setFailed(error);
   }
+
+  core.info(`Found most recent release: \`${latestRelease.tag_name}\``);
 
   core.setOutput('url', latestRelease.url);
   core.setOutput('assets_url', latestRelease.assets_url);
