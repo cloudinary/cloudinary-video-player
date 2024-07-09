173e304c04fb7561fc202daa08b678ef641499b1
shaylevi <shay.levi@cloudinary.com>
2024-07-09 10:54:15 +0300

    vp test: increase number of workers to 6

--- test/e2e/playwright.config.ts
+++ test/e2e/playwright.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     /* Retry on CI only */
     retries: process.env.CI ? 2 : 0,
     /* Opt out of parallel tests on CI. */
-    workers: 5,
+    workers: 6,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: [['list'], ['html', { open: 'never' }]],
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
