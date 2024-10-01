3e8d981964c10e8321a988f27bec2f1b7fac8078
jakubroch <jakub.roch@cloudinary.com>
2024-10-01 05:33:30 +0200

    feat: add url template for video player profiles

--- src/player.js
+++ src/player.js
@@ -2,22 +2,21 @@ import VideoPlayer from './video-player';
 import { defaultProfiles } from './config/profiles';
 import { isRawUrl } from './plugins/cloudinary/common';
 
-export const getProfile = async (cloudName, profile) => {
+export const getProfile = async (cloudName, profile, secureDistribution) => {
   if (Object.keys(defaultProfiles).includes(profile)) {
     return defaultProfiles[profile];
   }
 
-  if (isRawUrl(profile)) {
-    return await fetch(profile, { method: 'GET' }).then(res => res.json());
-  }
-
-  throw new Error('Custom profiles will be supported soon, please use one of default profiles: "cldDefault", "cldLooping" or "cldAdaptiveStream"');
+  const cloudinaryDomain = secureDistribution === 'res-staging.cloudinary.com' ? secureDistribution : 'res.cloudinary.com';
+  const profileUrl = isRawUrl(profile) ? profile :
+    `https://${cloudinaryDomain}/${cloudName}/_applet_/video_service/video_player_profiles/${profile}.json`;
+  return await fetch(profileUrl, { method: 'GET' }).then(res => res.json());
 };
 
 const player = async (elem, initOptions, ready) => {
   const { profile, ...otherInitOptions } = initOptions;
   try {
-    const profileOptions = profile ? await getProfile(otherInitOptions.cloud_name, profile) : {};
+    const profileOptions = profile ? await getProfile(otherInitOptions.cloud_name, profile, otherInitOptions.secure_distribution) : {};
     const options = Object.assign({}, profileOptions.playerOptions, otherInitOptions);
     const videoPlayer = new VideoPlayer(elem, options, ready);
 
