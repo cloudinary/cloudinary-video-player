fd911a61e74dec85bdc6cd7b3d588fb406bb24b6
Tsachi Shlidor <tsachi.shlidor@cloudinary.com>
2024-12-09 12:13:48 +0200

    chore: trigger release

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -13,6 +13,7 @@ import colors from './colors';
 import contextMenu from './context-menu';
 import floatingPlayer from './floating-player';
 import pacedTranscript from './paced-transcript';
+import srtTextTracks from './srt-text-tracks';
 import vttThumbnails from './vtt-thumbnails';
 
 // Lazy loaded plugins
@@ -20,7 +21,6 @@ import chapters from './chapters';
 import imaPlugin from './ima';
 import playlist from './playlist';
 import shoppable from './shoppable-plugin';
-import srtTextTracks from './srt-text-tracks';
 import styledTextTracks from './styled-text-tracks';
 import interactionAreas from './interaction-areas';
 
