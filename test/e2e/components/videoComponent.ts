845e1786a66cef8aafcd52aa4c5ef01e8977cfef
ShayLevi <61374483+ShayLevi@users.noreply.github.com>
2024-12-11 15:35:43 +0200

    me-17957: test if video on autoplay on scroll page is playing (#763)
    * vp test: test if video on autoplay on scroll page is playing
    
    * vp test: remove waitForTimeout and use expect toPass
    
    * vp test: remove waitForTimeout and use expect toPass
    
    * vp test: added BaseComponent and modified accordingly

--- test/e2e/components/videoComponent.ts
+++ test/e2e/components/videoComponent.ts
@@ -1,22 +1,20 @@
 import { Page } from '@playwright/test';
+import { BaseComponent } from './BaseComponent';
 
 /**
  * Video component
  */
-export class VideoComponent {
-    private page: Page;
-    private readonly videoSelector: string;
+export class VideoComponent extends BaseComponent{
 
     constructor(page: Page, videoSelector: string) {
-        this.page = page;
-        this.videoSelector = videoSelector;
+        super({ page, selector: videoSelector });
     }
 
     /**
      * Click the play button if necessary in case video is not autoplay
      */
     public async clickPlay(): Promise<void> {
-        const videoPlayButtonLocator = this.page.locator(`${this.videoSelector}/following-sibling::button[contains(@class, "vjs-big-play-button")]`);
+        const videoPlayButtonLocator = this.props.page.locator(`${this.props.selector}/following-sibling::button[contains(@class, "vjs-big-play-button")]`);
         // Click the play button to start the video
         return videoPlayButtonLocator.click();
     }
@@ -25,11 +23,11 @@ export class VideoComponent {
      * Checks if video element is paused
      */
     public async isPaused(): Promise<boolean> {
-        return this.page.evaluate((selector: string) => {
+        return this.props.page.evaluate((selector: string) => {
             console.log('Evaluating selector in browser context:', selector); // Logs selector in browser context
             const xpathResult = document.evaluate(selector, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
             const video = xpathResult.singleNodeValue as HTMLVideoElement | null;
             return video.paused;
-        }, this.videoSelector);
+        }, this.props.selector);
     }
 }
