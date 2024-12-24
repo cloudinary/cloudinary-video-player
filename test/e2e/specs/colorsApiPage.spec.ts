009145a4bf4d1b7f3a93c8f23cd774864e270e82
shaylevi <shay.levi@cloudinary.com>
2024-12-24 09:35:31 +0200

    vp test: modifying test steps description based on review comments

--- test/e2e/specs/colorsApiPage.spec.ts
+++ test/e2e/specs/colorsApiPage.spec.ts
@@ -11,16 +11,16 @@ vpTest(`Test if 3 videos on colors API page are playing as expected`, async ({ p
         await pomPages.mainPage.clickLinkByName(link.name);
         await waitForPageToLoadWithTimeout(page, 5000);
     });
-    await test.step('Validating that the first video is playing (in case isPause is false)', async () => {
+    await test.step('Validating that modified color video is playing (in case isPause is false)', async () => {
         expect(await pomPages.colorsApiPage.colorsApiColorSkinVideoComponent.validateVideoPaused(false));
     });
-    await test.step('Validating that the second video is playing (in case isPause is false)', async () => {
+    await test.step('Validating that dark skin video video is playing (in case isPause is false)', async () => {
         expect(await pomPages.colorsApiPage.colorsApiDarkSkinVideoComponent.validateVideoPaused(false));
     });
-    await test.step('Scroll until the third video element is visible', async () => {
+    await test.step('Scroll until light skin video element is visible', async () => {
         await pomPages.colorsApiPage.colorsApiLightSkinVideoComponent.locator.scrollIntoViewIfNeeded();
     });
-    await test.step('Validating that the third video is playing (in case isPause is false)', async () => {
+    await test.step('Validating that light skin video is playing (in case isPause is false)', async () => {
         await expect(async () => {
             expect(await pomPages.colorsApiPage.colorsApiLightSkinVideoComponent.validateVideoPaused(false));
         }).toPass({ intervals: [500], timeout: 3000 });
