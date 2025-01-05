63dc8d070d898a6857b638935d0edf437d509250
shaylevi <shay.levi@cloudinary.com>
2025-01-05 14:32:26 +0200

    vp test: test if video on playlist by tag page is playing

--- test/e2e/specs/playlistByTagPage.spec.ts
+++ test/e2e/specs/playlistByTagPage.spec.ts
@@ -6,7 +6,7 @@ import { ExampleLinkName } from '../testData/ExampleLinkNames';
 
 const link = getLinkByName(ExampleLinkName.PlaylistByTag);
 
-vpTest.only(`Test if video on playlist by tag page is playing as expected`, async ({ page, pomPages }) => {
+vpTest(`Test if video on playlist by tag page is playing as expected`, async ({ page, pomPages }) => {
     await test.step('Navigate to playlist by tag page by clicking on link', async () => {
         await pomPages.mainPage.clickLinkByName(link.name);
         await waitForPageToLoadWithTimeout(page, 5000);
