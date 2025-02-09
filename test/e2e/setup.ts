630be61b8f79147819e534c3e39dd4e7b9e68353
shaylevi <shay.levi@cloudinary.com>
2025-02-09 11:06:46 +0200

    vp test: create setup for esm preview

--- test/e2e/setup.ts
+++ test/e2e/setup.ts
@@ -1,15 +1,18 @@
 import { ExampleLinkType } from './types/exampleLinkType';
-import { expect, Page } from '@playwright/test';
+import { chromium, expect, Page } from '@playwright/test';
 import { ExampleLinkName } from './testData/ExampleLinkNames';
 import { ESM_URL } from './testData/esmUrl';
 
-async function globalSetup(page: Page) {
+async function globalSetup() {
     if (process.env.PREVIEW_URL) {
+        const browser = await chromium.launch();
+        const page = await browser.newPage();
         const link: ExampleLinkType = {
             name: ExampleLinkName.AdaptiveStreaming,
             endpoint: 'adaptive-streaming',
         };
         await waitForDeployPreviewUrl(link, page);
+        await browser.close();
     }
 }
 
@@ -21,6 +24,6 @@ async function waitForDeployPreviewUrl(link: ExampleLinkType, page: Page): Promi
         await page.goto(ESM_URL);
         const linkLocator = page.getByRole('link', { name: link.name, exact: true });
         await expect(linkLocator).toBeVisible({ timeout: 10000 });
-    }).toPass({ intervals: [1_000], timeout: 150000 });
+    }).toPass({ intervals: [1_000], timeout: 120000 });
 }
 export default globalSetup;
