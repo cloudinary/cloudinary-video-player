9fc9f7a5251529203e5b8ecbd0e88d61b6110578
jakub-roch <70572646+jakub-roch@users.noreply.github.com>
2025-02-27 12:40:26 +0100

    fix: add live stream error screen & auto refresh (#819)
    * fix: add live stream error screen & auto refresh

--- test/e2e/specs/NonESM/linksConsolErros.spec.ts
+++ test/e2e/specs/NonESM/linksConsolErros.spec.ts
@@ -22,7 +22,7 @@ for (const link of LINKS) {
  * Testing number of links in page.
  */
 vpTest('Link count test', async ({ page }) => {
-    const expectedNumberOfLinks = 37;
+    const expectedNumberOfLinks = 38;
     const numberOfLinks = await page.getByRole('link').count();
     expect(numberOfLinks).toBe(expectedNumberOfLinks);
 });
