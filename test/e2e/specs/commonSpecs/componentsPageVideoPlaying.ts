477dbb592b681dc77da7da60b2f20c49b20cd488
ShayLevi <61374483+ShayLevi@users.noreply.github.com>
2025-03-30 18:43:23 +0300

    vp test: test if video is playing on ESM components page (#838)

---
+++ test/e2e/specs/commonSpecs/componentsPageVideoPlaying.ts
@@ -0,0 +1,14 @@
+import { Page, test } from '@playwright/test';
+import { waitForPageToLoadWithTimeout } from '../../src/helpers/waitForPageToLoadWithTimeout';
+import PageManager from '../../src/pom/PageManager';
+import { ExampleLinkType } from '../../types/exampleLinkType';
+
+export async function testComponentsPageVideoIsPlaying(page: Page, pomPages: PageManager, link: ExampleLinkType) {
+    await test.step('Navigate to components page by clicking on link', async () => {
+        await pomPages.mainPage.clickLinkByName(link.name);
+        await waitForPageToLoadWithTimeout(page, 5000);
+    });
+    await test.step('Validating that components video is playing', async () => {
+        await pomPages.componentsPage.componentsVideoComponent.validateVideoIsPlaying(true);
+    });
+}
