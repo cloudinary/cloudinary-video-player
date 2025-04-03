d0fbf764aa437d4961fa0a2e9e90d7011bed5e11
ShayLevi <61374483+ShayLevi@users.noreply.github.com>
2025-04-03 12:39:37 +0300

    vp test: test if video is playing on ESM fluid layouts page (#842)

--- test/e2e/specs/ESM/esmFluidLayoutsPage.spec.ts
+++ test/e2e/specs/ESM/esmFluidLayoutsPage.spec.ts
@@ -2,9 +2,11 @@ import { vpTest } from '../../fixtures/vpTest';
 import { getLinkByName } from '../../testData/pageLinksData';
 import { ExampleLinkName } from '../../testData/ExampleLinkNames';
 import { testFluidLayoutsPageVideoIsPlaying } from '../commonSpecs/fluidLayoutsPageVideoPlaying';
+import { ESM_URL } from '../../testData/esmUrl';
 
 const link = getLinkByName(ExampleLinkName.FluidLayouts);
 
-vpTest(`Test if video on fluid layouts page is playing as expected`, async ({ page, pomPages }) => {
+vpTest(`Test if video on ESM fluid layouts page is playing as expected`, async ({ page, pomPages }) => {
+    await page.goto(ESM_URL);
     await testFluidLayoutsPageVideoIsPlaying(page, pomPages, link);
 });
