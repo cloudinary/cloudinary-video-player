1ba93cc96de4237ea62df9ab2afc28c0e02899e5
Tsachi Shlidor <tsachi.shlidor@cloudinary.com>
2025-05-27 16:50:55 +0300

    chore: e2e

--- test/e2e/testData/ExampleLinkNames.ts
+++ test/e2e/testData/ExampleLinkNames.ts
@@ -16,7 +16,6 @@ export enum ExampleLinkName {
     CustomErrors = 'Custom Errors',
     DisplayConfigurations = 'Display Configurations',
     DebugMode = 'Debug mode',
-    // ESModuleImports = 'ES Module Imports',
     FloatingPlayer = 'Floating Player',
     FluidLayouts = 'Fluid Layouts',
     ForceHLSSubtitles = 'Force HLS Subtitles',
