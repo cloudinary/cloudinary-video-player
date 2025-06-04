2fc1262661028aa5728dedaedc395be5491317ad
Tsachi Shlidor <tsachi.shlidor@cloudinary.com>
2025-06-04 17:06:03 +0300

    refactor: lazy loaded all bundle

--- src/index.all.js
+++ src/index.all.js
@@ -10,10 +10,15 @@ import cloudinary from './index.js';
 export * from './index.js';
 export * from './plugins/adaptive-streaming/adaptive-streaming.js';
 export * from './plugins/chapters/chapters.js';
+export * from './plugins/colors/colors.js';
 export * from './plugins/ima/ima.js';
 export * from './plugins/playlist/playlist.js';
 export * from './plugins/interaction-areas/interaction-areas.service.js';
 export * from './plugins/visual-search/visual-search.js';
+export * from './plugins/srt-text-tracks/srt-text-tracks.js';
+export * from './plugins/styled-text-tracks/styled-text-tracks.js';
+export * from './plugins/shoppable-plugin/shoppable-plugin.js';
 export * from './components/shoppable-bar/shoppable-widget.js';
+export * from './components/recommendations-overlay/recommendations-overlay.js';
 
 export default cloudinary;
