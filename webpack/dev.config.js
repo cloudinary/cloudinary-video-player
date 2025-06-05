3c45bea315cd352a08a0c258a15395384d2f0055
Tsachi Shlidor <tsachi.shlidor@cloudinary.com>
2025-06-05 17:13:51 +0300

    ci: purge jsdelivr cache and add version banner

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -3,13 +3,25 @@ const webpackCommon = require('./common.config');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const BannerPlugin = require('webpack/lib/BannerPlugin');
 
 const env = require('../env');
+
+const VERSION = JSON.stringify(process.env.npm_package_version);
 
 module.exports = merge(webpackCommon, {
   mode: 'development',
 
   plugins: [
+    new BannerPlugin({
+      banner: `/*!
+ * Cloudinary Video Player v${VERSION.replace(/"/g, '')}
+ * Built on ${new Date().toISOString()}
+ * https://github.com/cloudinary/cloudinary-video-player
+ */`,
+      entryOnly: false,
+      raw: true
+    }),
     new HtmlWebpackPlugin({
       inject: false,
       template: path.resolve(__dirname, '../docs/index.html')
