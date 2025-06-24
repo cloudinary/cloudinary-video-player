c85dcd0598403f3554d6c5be3f493f4308ba9b0e
refael-m <refael.mahpud@cloudinary.com>
2025-06-24 16:02:12 +0300

    update

--- test/e2e/playwright.config.ts
+++ test/e2e/playwright.config.ts
@@ -4,7 +4,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-    timeout: 150000,
+    timeout: 60000,
     /* Run tests in files in parallel */
     fullyParallel: true,
     /* Fail the build on CI if you accidentally left test.only in the source code. */
