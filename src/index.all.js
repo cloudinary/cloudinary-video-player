5a958cd15efd318cbec141c6871c8715d23dd9bb
jakubroch <jakub.roch@cloudinary.com>
2025-08-11 08:52:17 +0200

    feat: subtitles on demand

--- src/index.all.js
+++ src/index.all.js
@@ -15,7 +15,6 @@ export * from './plugins/ima/ima.js';
 export * from './plugins/playlist/playlist.js';
 export * from './plugins/interaction-areas/interaction-areas.service.js';
 export * from './plugins/visual-search/visual-search.js';
-export * from './plugins/srt-text-tracks/srt-text-tracks.js';
 export * from './components/shoppable-bar/shoppable-widget.js';
 export * from './components/recommendations-overlay/recommendations-overlay.js';
 
