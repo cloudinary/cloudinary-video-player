da1551fbfd35c7c484a153249237e56220d57b4f
Tsachi Shlidor <tsachi.shlidor@cloudinary.com>
2026-05-05 12:24:33 +0300

    fix: improve error message display (#1042)
    * fix: improved error messages display
    
    * chore: not every error needs a refresh

--- src/components/error-display/error-display.js
+++ src/components/error-display/error-display.js
@@ -9,12 +9,18 @@ class CldErrorDisplay extends ErrorDisplay {
     if (!error) return '';
 
     const wrapper = videojs.dom.createEl('div', { className: 'cld-error-display-content' });
-    const msg = videojs.dom.createEl('span', { className: 'cld-error-message' });
+    const header = videojs.dom.createEl('div', { className: 'cld-error-header' });
+    const title = videojs.dom.createEl('span', { className: 'cld-error-title' }, {}, this.localize('Something went wrong'));
+    const msg = videojs.dom.createEl('div', { className: 'cld-error-message' });
+
+    header.appendChild(title);
+
     if (error.isHtml) {
       msg.innerHTML = this.localize(error.message);
     } else {
       msg.textContent = this.localize(error.message);
     }
+    wrapper.appendChild(header);
     wrapper.appendChild(msg);
 
     const refreshLink = msg.querySelector('.cld-error-refresh');
